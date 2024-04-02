0b7fa9738013cbc430cc38b432c09304a73b596d
skmake <matboe0@gmail.com>
2024-04-02 15:12:55 +0200

    katt og sont

--- immc_katt.py
+++ immc_katt.py
@@ -1,5 +1,10 @@
 import math 
 
+# Tid ( hjemme, vekter lite)
+# Bolig ( ute/inne, vekter mye fordi katten mjauer)
+# Barn (vekter middels foodi ansvar og)
+# Lager score paa disse tre for katte modellen mjau.
+
 c_responsibility_weight = 50
 c_time_weight = 30
 c_adaptability_weight = 20
@@ -15,20 +20,22 @@ class Household():
     def calculate(self):
         responsibility = self.calculate_responsibility(0.7, 3)
         adaptability = self.calculate_adaptability()
-        time = self.calculate_time(1/6)
+        time = self.calculate_time()
         return (time * c_time_weight) + (responsibility * c_responsibility_weight) + (adaptability * c_adaptability_weight)
         
     def calculate_responsibility(self, personality, amount_of_kids):
         return 1
 
-    def calculate_adaptability(self):
-        return 1
+    def calculate_adaptability(self, amount_of_kids, house_type):
+        score = 0
+        if amount_of_kids == 0:
+            score += 0.5
         
-    def calculate_time(self, time): # avg time per day in hours
+    def calculate_time(self): # avg time per day in hours
         # Cats need min 30 min
         # https://www.catster.com/guides/how-much-attention-does-a-cat-need/
-        assert time <= 24
-        score = float(100.2584 / (1 + 9.2034*(math.exp(-5.2851*time))))
+        assert self.time_at_home <= 24
+        score = float(100.2584 / (1 + 9.2034*(math.exp(-5.2851*self.time_at_home))))
         if score > 100:
             score = 100
         elif score < 0:
