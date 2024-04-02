9507ca9cb1978b4bed4e09f0f78029c724aad756
skmake <matboe0@gmail.com>
2024-04-02 10:39:14 +0200

    first

---
+++ immc.py
@@ -0,0 +1,10 @@
+import math
+
+def calculate_cat_preparedness(annual_gross_household_income, spare_time, other_animals, property_area):
+    print(str((annual_gross_household_income + spare_time + other_animals + property_area) / 4) + "%" + " +- " + math. + "%")
+
+def main():
+    calculate_cat_preparedness(100, 234, 0, 1)
+
+if __name__ == "__mian__":
+    main()
